594750befc94705ad42e27cf355147a0d82543c2
Alejandro Canales <calexluis@icloud.com>
2026-04-24 13:11:19 +0000

    Fix: Player init moved outside of looop

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH//2,SCREEN_HEIGHT//2)
     while True:
         log_state()
         for event in pygame.event.get():
             pass
         screen.fill("black")
-        player = Player(SCREEN_WIDTH//2,SCREEN_HEIGHT//2)
         player.update(dt)
         player.draw(screen)
         pygame.display.flip()
