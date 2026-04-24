d743d80e3b57e670c4a988b11c801b9737c01424
Alejandro Canales <calexluis@icloud.com>
2026-04-24 05:38:16 -0700

    Updated dt

--- main.py
+++ main.py
@@ -16,8 +16,7 @@ def main():
             pass
         screen.fill("black")
         pygame.display.flip()
-        dt = clock.tick(60)
-        print(dt)
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
