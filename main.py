684d6519073a31802d19342c3fbfbd7711bde007
Alejandro Canales <calexluis@icloud.com>
2026-04-24 12:40:56 +0000

    Fix: Removed Noframe

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT),pygame.NOFRAME)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
     while True:
