17130c4ffac1f8b2ce21b8595f2a15f924e34ab0
Alejandro Canales <calexluis@icloud.com>
2026-04-24 13:21:30 +0000

    Fix: Movement now goes the right way

--- player.py
+++ player.py
@@ -36,6 +36,6 @@ class Player(CircleShape):
         if keys[pygame.K_d]:
             self.rotate(dt)
         if keys[pygame.K_s]:
-            self.move(dt)
-        if keys[pygame.K_w]:
             self.move(-dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
